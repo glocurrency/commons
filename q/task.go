3b7ea3814dcb90aa5a87d509be4fbfe30dd7c6ae
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-11-17 16:36:00 +0000

    feat: marshall task data

--- q/task.go
+++ q/task.go
@@ -1,5 +1,10 @@
 package q
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Task represents a unit of work to be performed.
 type Task struct {
 	// typename indicates the type of task to be performed.
@@ -20,6 +25,17 @@ func NewTask(typename string, payload []byte, opts ...TaskOption) *Task {
 		payload:  payload,
 		opts:     opts,
 	}
+}
+
+// NewTaskWithJSON returns a new Task given a type name and JSON payload that will be marshaled.
+// Options can be passed to configure task processing behavior.
+func NewTaskWithJSON(typename string, payload interface{}, opts ...TaskOption) (*Task, error) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal payload: %w", err)
+	}
+
+	return NewTask(typename, data, opts...), nil
 }
 
 type TaskInfo struct {
