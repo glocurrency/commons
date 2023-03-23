857a692c4d2f08a31264f80181bfbfa26fcaafa8
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-03-23 13:12:41 +0000

    test: TestParseParamUUID

---
+++ binding/binding_test.go
@@ -0,0 +1,58 @@
+package binding_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/glocurrency/commons/binding"
+	"github.com/glocurrency/commons/logger"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	logger.Log().SetOutput(io.Discard)
+	gin.SetMode(gin.ReleaseMode)
+	os.Exit(m.Run())
+}
+
+func TestParseParamUUID(t *testing.T) {
+	tests := []struct {
+		name       string
+		paramName  string
+		paramValue string
+		wantErr    bool
+	}{
+		{"valid uuid", "id", uuid.NewString(), false},
+		{"invalid uuid", "id", "invalid", true},
+		{"invalid param name", "xyz", uuid.NewString(), true},
+	}
+
+	for i := range tests {
+		test := tests[i]
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			req := httptest.NewRequest(http.MethodGet, "/"+test.paramValue, nil)
+
+			w := httptest.NewRecorder()
+
+			router := gin.New()
+			router.GET("/:id", func(ctx *gin.Context) {
+				got, err := binding.ParseParamUUID(ctx, test.paramName)
+				if test.wantErr {
+					assert.Error(t, err)
+					return
+				} else {
+					assert.NoError(t, err)
+					assert.Equal(t, test.paramValue, got.String())
+				}
+			})
+			router.ServeHTTP(w, req)
+		})
+	}
+}
