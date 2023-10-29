fa9d65dfef67c79cd793b33f42a38c90e13c8f84
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-10-29 14:46:36 +0000

    feat: validation

---
+++ validation/validation.go
@@ -0,0 +1,25 @@
+package validation
+
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var alphaNumericSpaceRegex = regexp.MustCompile(`^[a-zA-Z0-9\s]+$`)
+
+func ValidateAlphaNumSpace(fl validator.FieldLevel) bool {
+	return alphaNumericSpaceRegex.MatchString(fl.Field().String())
+}
+
+var alphaNumericSpaceDashRegex = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`)
+
+func ValidateAlphaNumSpaceDash(fl validator.FieldLevel) bool {
+	return alphaNumericSpaceDashRegex.MatchString(fl.Field().String())
+}
+
+var bankSupportedRegex = regexp.MustCompile(`^[a-zA-Z0-9\/\-?:().,"+\s]+$`)
+
+func ValidateBankSupported(fl validator.FieldLevel) bool {
+	return bankSupportedRegex.MatchString(fl.Field().String())
+}
