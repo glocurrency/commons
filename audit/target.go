efb9b2e039e98bf61da1899665e932737de704df
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-01-07 15:26:49 +0000

    feat: actors

--- audit/target.go
+++ audit/target.go
@@ -2,6 +2,13 @@ package audit
 
 import "github.com/google/uuid"
 
+const (
+	ActorTypeServer  = "SERVER"
+	ActorTypeUser    = "USER"
+	ActorTypeMember  = "MEMBER"
+	ActorTypeWebhook = "WEBHOOK"
+)
+
 type Target interface {
 	GetID() uuid.UUID
 	GetAuditTargetType() string
