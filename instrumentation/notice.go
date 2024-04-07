3e334f16dc7ccd928632d3a5946b7867be95e44d
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-04-07 12:24:27 +0100

    chore: one more sample

--- instrumentation/notice.go
+++ instrumentation/notice.go
@@ -9,6 +9,7 @@ import (
 
 // TODO: depending on ctx, insert GCP related info.
 // sample: https://github.com/ncruces/go-gcp/blob/master/glog/glog.go
+// sample: https://github.com/googleapis/google-cloud-go/blob/main/logging/logging.go
 // fields: https://cloud.google.com/logging/docs/structured-logging#special-payload-fields
 
 func NoticeError(ctx context.Context, err error, msg string, opts ...NoticeOption) {
