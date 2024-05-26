b14e20998c7462dc2bdc48b770984828f56859b6
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-05-26 18:17:14 +0100

    fix!: typo

--- storage/storage.go
+++ storage/storage.go
@@ -9,20 +9,22 @@ import (
 type Storage interface {
 	Signer
 	Uploader
-	Downloaded
+	Downloader
 }
 
 type Signer interface {
-	// GetSignedURL returns a signed URL. The URL is valid for the given duration
+	// GetSignedURL returns a signed URL, and an error if the operation fails.
 	GetSignedURL(context.Context, string, time.Duration) (string, error)
 }
 
 type Uploader interface {
-	// Upload uploads a file. Returns an error if the upload fails
+	// Upload uploads a file.
+	// Returns an error if the upload fails.
 	Upload(context.Context, string, io.ReadSeeker) error
 }
 
-type Downloaded interface {
-	// Download downloads a file. Returns content, content type and error
+type Downloader interface {
+	// Download downloads a file.
+	// Returns content, content type and error if the download fails.
 	Download(ctx context.Context, name string) ([]byte, string, error)
 }
